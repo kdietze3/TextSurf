1de7192194ac86a171138f931406886494da462e
Kevin Dietze <kdietze3@gmail.com>
2012-12-13 03:08:38 -0500

    First Commit!
    Lets get this party started.

---
+++ src/com/dietze/textsurf/BrowserActivity.java
@@ -0,0 +1,50 @@
+package com.dietze.textsurf;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class BrowserActivity extends Activity {
+
+	TextView text;
+	WebView webview;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//Initialize the Web and TextViews
+		webview = (WebView) findViewById(R.id.webView1);
+		text = (TextView) findViewById(R.id.editText1);
+		
+		//Initialize Go Button last to avoid nullPointerException
+		Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(urlListener);
+		
+		setContentView(R.layout.activity_browser);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_browser, menu);
+		return true;
+	}
+	
+	private OnClickListener urlListener = new OnClickListener(){
+		public void onClick(View v){
+			//Do something
+			String url = text.getText().toString();
+			webview.loadUrl(url);
+			
+		}
+	};
+	
+	
+
+}
